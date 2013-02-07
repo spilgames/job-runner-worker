811974f1f43902e177a41e66342642fae8215bb0
Orne Brocaar <info@brocaar.com>
2013-02-07 11:38:14 +0100

    Fix test.

--- job_runner_worker/tests/unit/test_config.py
+++ job_runner_worker/tests/unit/test_config.py
@@ -32,7 +32,7 @@ class ModuleTestCase(unittest.TestCase):
             'concurrent_jobs': '4',
             'ws_server_port': '5555',
             'broadcaster_server_port': '5556',
-            'reconnect_after_inactivity': str(60 * 5),
+            'reconnect_after_inactivity': str(60 * 10),
         })
         config_mock.read.assert_called_once_with('/path/to/settings')
         self.assertEqual(config_mock, config)
